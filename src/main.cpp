98c7f8f97517a87b35d7eb6fe2ef7a399c99254f
gogonona384_crypto <gogonona3@example.com>
2026-09-13 04:52:54 +0300

    Day 2 complete: Test polymorphism in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 #include "../include/MediaItem.h"
+#include "../include/Song.h"
+#include "../include/Podcast.h"
 
 using namespace std;
 
 int main() {
+    MediaItem* items[2];
+
+    items[0] = new Song("Bahebak", "Amr Diab", 225, "Pop");
+    items[1] = new Podcast("Ep.12 Careers", "Ahmed Samir", 1680, 12);
+
+    cout << "--- Testing Polymorphism ---\n\n";
+
+    for (int i = 0; i < 2; i++) 
+    {
+        items[i]->play();
+        cout << endl;
+    }
+
+    for (int i = 0; i < 2; i++) 
+    {
+        delete items[i];
+    }
     cout << "Project Setup Completed Successfully!" << endl;
     return 0;
 }
