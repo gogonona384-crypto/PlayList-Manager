f9134029750e56a8948516c4552fdd1b00f9ff88
gogonona384_crypto <gogonona3@example.com>
2026-09-13 03:43:06 +0300

    Add initial header files and main structure

---
+++ include/MediaItem.h
@@ -0,0 +1,28 @@
+#ifndef MEDIAITEM_H
+#define MEDIAITEM_H
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class MediaItem {
+protected:
+    string title;
+    int duration; // بالثواني أو دقائق
+    int playCount;
+
+public:
+    MediaItem(string t = "", int d = 0) : title(t), duration(d), playCount(0) {}
+    virtual ~MediaItem() {}
+
+    // Pure Virtual Function
+    virtual void play() = 0;
+    virtual void getInfo() const = 0;
+
+    string getTitle() const { return title; }
+    int getDuration() const { return duration; }
+    int getPlayCount() const { return playCount; }
+    void incrementPlayCount() { playCount++; }
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../include/MediaItem.h"
+
+using namespace std;
+
+int main() {
+    cout << "Project Setup Completed Successfully!" << endl;
+    return 0;
+}
